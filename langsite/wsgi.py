8f460bd2bd01d3f3da2fc94dc4fd94c973fd326b
sowrd299 <ryan.ward46@gmail.com>
2019-04-24 11:11:35 -0700

    more server conf

--- langsite/wsgi.py
+++ langsite/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/var/www/vhosts/LangSite')
+sys.path.append('/var/www/vhosts/LangSite/venv/lib/python3.4/site-packages')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'langsite.settings')
 
 application = get_wsgi_application()
