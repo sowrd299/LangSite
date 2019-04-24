fbfdc23e249fb33e824cb04ef758a78e10733657
sowrd299 <ryan.ward46@gmail.com>
2019-04-24 13:00:10 -0700

    Slight improvements to the database

--- articles/views.py
+++ articles/views.py
@@ -2,26 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.views.generic.base import TemplateView
+from django.core import exceptions
 
-# a hap-dash conception of "topics"
-# TODO: this really belongs 1) in model and 2) in a data base
-class Topic():
-    def __init__(self, name, subtopics):
-        self.name = name
-        self.subtopics = subtopics
-
-    # this allows us to ref the object itself in a template to get it's name
-    def __str__(self):
-        return self.name
-
-topics = {
-    Topic('ELD' , set()),
-    Topic('Dual Language Imersion Schools' , {'Spanish', 'Croatian'}),
-    Topic('The Value of Esperanto' , {'The Esperanto Thing is a Joke'}),
-    Topic('English Language Standardized Tests' , {'RIP STAR Test (not really)'}),
-}
-
-topic_lookup = { t.name : t for t in topics }
+from .models import *
 
 # the actual view 
 class ArticleView(TemplateView):
@@ -30,6 +13,10 @@ class ArticleView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['article_id'] = topic_lookup[kwargs['article_id']]
-        context['topics'] = topics
+        try:
+            context['article_id'] = Topic.objects.get(name=kwargs['article_id'])
+        except exceptions.ObjectDoesNotExist:
+            # non-existant articles can be handled gracefully by the template
+            context['article_id'] = None
+        context['topics'] = Topic.objects.all()
         return context
